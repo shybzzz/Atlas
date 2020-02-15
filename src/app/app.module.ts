bfa60aa022c86f2fa5a68fd9947814802cd01a87
shybzzz <rostykrom@yahoo.com>
2020-02-15 07:23:28 +0200

    moved angular to 8 version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 import {
   MatIconModule,
   MatButtonModule,
   MatMenuModule,
   MatProgressBarModule
-} from '@angular/material';
+} from "@angular/material";
 
-import { AgmCoreModule } from '@agm/core';
+import { AgmCoreModule } from "@agm/core";
 
-import config from '../assets/config.json';
-import { HttpClientModule } from '@angular/common/http';
+import config from "../assets/config.json";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent],
@@ -29,7 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatMenuModule,
     MatProgressBarModule,
     AgmCoreModule.forRoot({
-      apiKey: config.googleMapsApiKey
+      apiKey: ''
     }),
     HttpClientModule
   ],
