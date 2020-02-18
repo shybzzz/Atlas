23d952ac7cc747de1af20e8431ce49887a77b99c
shybzzz <rostykrom@yahoo.com>
2020-02-18 07:41:56 +0200

    basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,43 @@
 import { AtlasApiService } from './api/atlas-api/atlas-api.service';
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener
+} from '@angular/material/tree';
+import { FlatTreeControl } from '@angular/cdk/tree';
+
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Fruit',
+    children: [{ name: 'Apple' }, { name: 'Banana' }, { name: 'Fruit loops' }]
+  },
+  {
+    name: 'Vegetables',
+    children: [
+      {
+        name: 'Green',
+        children: [{ name: 'Broccoli' }, { name: 'Brussels sprouts' }]
+      },
+      {
+        name: 'Orange',
+        children: [{ name: 'Pumpkins' }, { name: 'Carrots' }]
+      }
+    ]
+  }
+];
+
+/** Flat node with expandable and level information */
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,13 +45,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  constructor(private atlasApiService: AtlasApiService) {
+    this.dataSource.data = TREE_DATA;
+  }
   title = 'Atlas';
 
   headerConfig: { logo: string; url: string };
   messages = {};
   styles;
 
-  constructor(private atlasApiService: AtlasApiService) {}
+  shownLayers = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi'
+  ];
+
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level,
+    node => node.expandable
+  );
+  // tslint:disable-next-line: variable-name
+  private _transformer = (node: FoodNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level
+    };
+  };
+
+  // tslint:disable-next-line: member-ordering
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    node => node.level,
+    node => node.expandable,
+    node => node.children
+  );
+
+  // tslint:disable-next-line: member-ordering
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  // tslint:disable-next-line: variable-name
 
   ngOnInit() {
     this.atlasApiService.getMessages().subscribe(m => {
@@ -25,4 +101,18 @@ export class AppComponent implements OnInit {
       this.styles = appSettings.googleMapStyles;
     });
   }
+
+  onMapReady($event) {
+    console.log('onMapReady', $event);
+  }
+
+  onStatusChange($event) {
+    console.log('onStatusChange', $event);
+  }
+
+  dropLayer(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.shownLayers, event.previousIndex, event.currentIndex);
+  }
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
-import { AgmCoreModule } from "@agm/core";
+import { AgmCoreModule } from '@agm/core';
 
-import config from "../assets/config.json";
-import { HttpClientModule } from "@angular/common/http";
+import config from '../assets/config.json';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatTreeModule } from '@angular/material/tree';
 
 @NgModule({
   declarations: [AppComponent],
@@ -26,7 +30,12 @@ import { HttpClientModule } from "@angular/common/http";
     MatButtonModule,
     MatMenuModule,
     MatProgressBarModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    DragDropModule,
+    MatTreeModule,
     AgmCoreModule.forRoot({
+      ...config,
       apiKey: ''
     }),
     HttpClientModule
